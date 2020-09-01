bae8713228df11d4d8c6ccdba6e026830ea5c0ca
marcelosperalta <marcelosperalta@gmail.com>
2020-09-01 22:13:51 +0200

    To-do App update

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,21 +2,22 @@ import express, { Express } from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import todoRoutes from './routes'
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser'
 
 const app: Express = express()
 
 const PORT: string | number = process.env.PORT || 4000
 
-app.use(bodyParser());
+app.use(bodyParser())
 app.use(cors())
 app.use(todoRoutes)
 
+// replace the host "cluster0.xo006.mongodb.net" with the address of your host generated in MongoDB Atlas.
+// https://docs.mongodb.com/manual/reference/connection-string/
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.xo006.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.set('useFindAndModify', false)
-
 
 mongoose
 .connect(uri, options)
--- server/src/models/todo.ts
+++ server/src/models/todo.ts
@@ -1,25 +1,21 @@
-import { ITodo } from "./../types/todo"
-import { model, Schema } from "mongoose"
+import { ITodo } from './../types/todo';
+import { model, Schema } from 'mongoose'
 
-const todoSchema: Schema = new Schema(
-    {
-
-        name: {
-            type: String,
-            required: true,
-        },
-
-        description: {
-            type: String,
-            required: true,
-        },
-
-        status: {
-            type: Boolean,
-            required: true,
-        },
+const todoSchema: Schema = new Schema({
+    name: {
+        type: String,
+        required: true
     },
-    { timestamps: true}
-)
 
-export default model<ITodo>("Todo", todoSchema)
+    description: {
+        type: String,
+        required: true
+    },
+
+    status: {
+        type: Boolean,
+        required: true
+    }
+}, { timestamps: true })
+
+export default model<ITodo>('Todo', todoSchema)
