bf6a286721e04335ecad260a9f1a69b439327d92
marcelosperalta <marcelosperalta@gmail.com>
2020-09-01 02:01:47 +0200

    To-do App update

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const PORT: string | number = process.env.PORT || 4000
 app.use(cors())
 app.use(todoRoutes)
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-01.xo006.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.set("useFindAndModify", false)
 
